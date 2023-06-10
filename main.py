69e2a4f43487413fe83e2a7e0675d73612c9bb36
ndichu52 <134836329+ndichu52@users.noreply.github.com>
2023-06-10 12:15:13 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,74 @@
+print("hello world")
+print("my name is ian")
+if 10>3:
+
+    print("ten is greater than three")
+#variables
+x = 5
+y = "this is variable y"
+z=0.8
+z=40
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+#assigning many values to multiple variables
+r,t,s ="ian", "nate", "brian"
+print(r)
+print(s)
+print(t)
+#assign one value to multiple variables
+a = b = c = "volvo"
+print(a)
+print(b)
+print(c)
+#unpack a collection
+vehicles=["volvo","Toyota","bmw"]
+q,w,e=vehicles
+
+print(q)
+print(w)
+print(e)
+# numbers
+y=100
+p=25
+s=y+p #addition
+d=y-p#subtraction
+f=y*p#multiplication
+g=y/p
+print(s)
+print(d)
+print(f)
+print(g)
+#imputing numbers and doing calculation
+a= int(input("enter first numbers"))
+b=int(input("enter second number"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print(c)
+print(d)
+print(e)
+print(f)
+print("addition of two numbers",c)
+print("subtraction of two numbers",d)
+print("multiplication of two numbers",e)
+print("division of two numbers",f)
+# challenge
+name=input("enter your name?")
+color=input("what is your favourite color")
+print(name,"like that color",color)
+a=int(input("enter year of birth"))
+b=int(input("the year today"))
+c=b-a
+print("subtraction of two numbers",c)
+
+
+
+
+
+
+
